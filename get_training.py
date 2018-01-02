86fa02fcd8231d7660a28cb7b451a21d4d892e96
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-01-02 16:08:49 -0600

    Make fully Python 3 compatible

--- get_training.py
+++ get_training.py
@@ -1,17 +1,18 @@
 from utils import load_config
 import time
-import urllib
+import urllib.request
+
 import os
 
 config = load_config.get_config()
 
 def download_image(state):
     for i in config['webcams']:
-        image = urllib.URLopener()
         timestamp = time.time()
-        image.retrieve(i, '{state}{timestamp}.jpg'.format(state=state, timestamp=timestamp))
+        filename = '{state}_{timestamp}.jpg'.format(state=state, timestamp=timestamp)
+        urllib.request.urlretrieve(i, filename)
 
 if __name__ == '__main__':
-    state = raw_input('State> ')
+    state = input('State> ')
     download_image(state)
     print('Done.')
