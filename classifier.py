ae3ca0fd2f1a8e5c23e3abcdffab644b48c5c142
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-03-30 08:21:31 -0400

    Add straightforward motion detection using simple average color channel deltas

--- classifier.py
+++ classifier.py
@@ -1,5 +1,30 @@
 import tensorflow as tf
+debug = False
 
-def getStatus(frame):
-    print('To be built...')
-    return None
+def getStatus(frame, lastStatus):
+    global debug
+    averageR = 0
+    averageG = 0
+    averageB = 0
+    added = 0
+    for i in frame:
+        averageR = averageR*added + i[0]
+        averageG = averageG*added + i[1]
+        averageB = averageB*added + i[2]
+        added += 1
+        averageR /= added
+        averageG /= added
+        averageB /= added
+    if debug:
+        print('Red: ', averageR)
+        print('Green: ', averageG)
+        print('Blue: ', averageB)
+    currentState = 'normal'
+    # Compare average deltas to detect movement or environment changes
+    threshold = 4
+    if not lastStatus == None:
+        if (abs(lastStatus['channels']['red'] - averageR) > threshold or
+            abs(lastStatus['channels']['green'] - averageG) > threshold or
+            abs(lastStatus['channels']['blue'] - averageB) > threshold):
+            currentState = 'motion'
+    return { 'state': currentState, 'channels' : { 'red': averageR, 'green': averageG, 'blue': averageB } }
--- feed.py
+++ feed.py
@@ -11,14 +11,17 @@ config = load_config.get_config()
 def grabFrames():
     to_return = []
     for i in config['webcams']:
-        response = r.get(i)
+        response = r.get(i.replace('{time}', str(int(time.time()))))
         img = Image.open(BytesIO(response.content))
         raw_data = list(img.getdata())
         to_return += [raw_data]
     return to_return
 
+lastStatus = None
 def assessStatus(frame):
-    status = classifier.getStatus(frame)
+    global lastStatus
+    status = classifier.getStatus(frame, lastStatus)
+    lastStatus = status
     return status
 
 def counterMeasure():
@@ -32,6 +35,7 @@ while not finished:
     frames = grabFrames()
     for i in frames:
         status = assessStatus(i)
+        print(status)
         if status == 'risk':
             counterMeasure()
     time.sleep(delay)
